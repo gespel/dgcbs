f0356117f6f76847c0716332b1e8b38178b1e8b4
gespel <heimbrodt@uni-potsdam.de>
2025-03-05 12:02:39 +0100

    other formating

--- vars/checkIfWorkerNodeUp.groovy
+++ vars/checkIfWorkerNodeUp.groovy
@@ -1,5 +1,5 @@
 def call(name, zone) {
-    def command = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances describe ${name} --zone ${zone} --format='get(status)'"
+    def command = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances describe " + name + " --zone " + zone + " --format='get(status)'"
     
     def process = command.execute()
     process.waitFor()
