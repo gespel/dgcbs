44cc831e3bdc1626cb4abd6d266547babe0643ef
gespel <heimbrodt@uni-potsdam.de>
2025-03-05 11:23:27 +0100

    working sample

--- vars/checkForWorker.groovy
+++ vars/checkForWorker.groovy
@@ -2,10 +2,11 @@ def call(name) {
     def runningJobs = []
     jenkins.model.Jenkins.instance.nodes.each { node ->
         if (node.name == name) {
-            runningJobs = node.computer.executors.findAll { it.isBusy() }
+            runningJobs = node.computer.executors.findAll { 
+                it.isBusy() 
+            }
         }
     }
     
-    // Gib die Anzahl der laufenden Jobs auf diesem Knoten aus
     echo "Es laufen ${runningJobs.size()} Jobs auf dem Knoten ${name}."
 }
