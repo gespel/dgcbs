edd0fc6f7b976aabdb10100d6905715cda679589
gespel <heimbrodt@uni-potsdam.de>
2025-03-05 12:09:01 +0100

    testing

--- vars/checkIfWorkerNodeUp.groovy
+++ vars/checkIfWorkerNodeUp.groovy
@@ -1,6 +1,7 @@
 def call(name, zone) {
     echo "Checking for ${name} in zone ${zone}"
-    def command = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances describe " + name + " --zone " + zone + " --format=\"get(status)\""
+    //def command = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances describe " + name + " --zone " + zone + " --format=\"get(status)\""
+    def commang = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances list"
     echo command
     def process = command.execute()
     process.waitFor()
