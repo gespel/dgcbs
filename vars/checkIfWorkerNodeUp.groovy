2aa9945f17a4c2e0e864f41c37488f8ba0423284
gespel <heimbrodt@uni-potsdam.de>
2025-03-05 12:13:00 +0100

    further testing

--- vars/checkIfWorkerNodeUp.groovy
+++ vars/checkIfWorkerNodeUp.groovy
@@ -1,17 +1,19 @@
 def call(name, zone) {
     echo "Checking for ${name} in zone ${zone}"
-    def command = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances describe " + name + " --zone " + zone + " --format=\"get(status)\""
+    //def command = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances describe " + name + " --zone " + zone + " --format=\"get(status)\""
     //def command = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances list"
-    echo command
-    def process = command.execute()
-    process.waitFor()
+    //echo command
+    //def process = command.execute()
+    //process.waitFor()
 
-    if (process.exitValue() != 0) {
-        echo "Fehler beim Ausführen des Befehls: ${process.err.text}"
-        return false
-    }
+    //if (process.exitValue() != 0) {
+    //    echo "Fehler beim Ausführen des Befehls: ${process.err.text}"
+    //    return false
+    //}
 
-    def status = process.text
+    //def status = process.text
+    def status = sh(script: "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances describe ${name} --zone ${zone} --format=\"get(status)\"",
+                    returnStdout: true).trim()
     echo "Status is: " + status
 
     if (status == "RUNNING") {
