a4c86598865df1f155c5ba08890d435346ade39f
gespel <heimbrodt@uni-potsdam.de>
2025-03-05 17:34:01 +0100

    refactor

--- vars/startWorkerNode.groovy
+++
@@ -1,6 +0,0 @@
-def call(name, zone) {
-    echo "Starting worker ${name} in zone ${zone}"
-    def status = sh(script: "", returnStdout: true).trim()
-    echo "Status is: " + status
-    return status
-}
