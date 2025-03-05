306a420bb80dec5d5371d61b1c16cdac78036443
gespel <heimbrodt@uni-potsdam.de>
2025-03-05 16:46:36 +0100

    class proto

--- src/DGCBSDaemon.groovy
+++ src/DGCBSDaemon.groovy
@@ -2,4 +2,17 @@ class DGCBSDaemon {
     public DGCBSDaemon() {
         println("Creating a new dynamic gcloud build system daemon")
     }
+
+    public int getNumJobsOfWorker(name) {
+        def runningJobs = []
+        jenkins.model.Jenkins.instance.nodes.each { node ->
+            if (node.name == name) {
+                runningJobs = node.computer.executors.findAll { 
+                    it.isBusy() 
+                }
+            }
+        }
+        
+        return runningJobs.size()
+    }
 }
