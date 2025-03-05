2db942b6626f40085308f83066fb40a527df3e58
gespel <heimbrodt@uni-potsdam.de>
2025-03-05 12:14:27 +0100

    using sh now

--- vars/checkIfWorkerNodeUp.groovy
+++ vars/checkIfWorkerNodeUp.groovy
@@ -1,17 +1,5 @@
 def call(name, zone) {
     echo "Checking for ${name} in zone ${zone}"
-    //def command = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances describe " + name + " --zone " + zone + " --format=\"get(status)\""
-    //def command = "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances list"
-    //echo command
-    //def process = command.execute()
-    //process.waitFor()
-
-    //if (process.exitValue() != 0) {
-    //    echo "Fehler beim Ausführen des Befehls: ${process.err.text}"
-    //    return false
-    //}
-
-    //def status = process.text
     def status = sh(script: "/home/jenkins/google-cloud-sdk/bin/gcloud compute instances describe ${name} --zone ${zone} --format=\"get(status)\"",
                     returnStdout: true).trim()
     echo "Status is: " + status
