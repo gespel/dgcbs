8e5d7c9095291a7e5e4ae4afcd87821e304bf784
gespel <heimbrodt@uni-potsdam.de>
2025-03-11 12:21:49 +0100

    debugging

--- src/DGCBSDaemon.groovy
+++ src/DGCBSDaemon.groovy
@@ -69,7 +69,7 @@ class DGCBSDaemon {
     }
 
     public void updateServers(ArrayList<ArrayList<String>> workerContainers) {
-        this.servers[0].addNode(new DynamicNode(wc[0], wc[1], wc[2]))
+        this.servers[0].addNode(new DynamicNode(workerContainers[0][0], workerContainers[0][1], workerContainers[0][2]))
         for(wc in workerContainers) {
             for(s in this.servers) {
                 s.addNode(new DynamicNode(wc[0], wc[1], wc[2]))
