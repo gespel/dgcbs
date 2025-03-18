00794be62b9d4c567e42dfb46747fe857330de6f
gespel <heimbrodt@uni-potsdam.de>
2025-03-18 09:54:18 +0100

    added execute function

--- src/InstanceCreator.groovy
+++ src/InstanceCreator.groovy
@@ -5,7 +5,7 @@ class InstanceCreator {
 
     public String executeGCloudCommand(String command) {
         def sout = new StringBuilder(), serr = new StringBuilder()
-        def proc = '/var/lib/jenkins/google-cloud-sdk/bin/gcloud $command'.execute()
+        def proc = "/var/lib/jenkins/google-cloud-sdk/bin/gcloud ${command}".execute()
         proc.consumeProcessOutput(sout, serr)
         proc.waitForOrKill(100000)
         return "$sout $serr"
