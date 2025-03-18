dca94dcd28476ffc3823ab4987d7d8ea3ae4e395
gespel <heimbrodt@uni-potsdam.de>
2025-03-18 11:55:50 +0100

    added cwd

--- src/InstanceCreator.groovy
+++ src/InstanceCreator.groovy
@@ -1,12 +1,13 @@
 import JenkinsTerraform
 
 class InstanceCreator {
-    public InstanceCreator() {
-
+    private String cwd;
+    public InstanceCreator(String cwd) {
+        this.cwd = cwd
     }
 
     public String createInstance(String name) {
-        def jtf = new JenkinsTerraform("workspace/createNewVM/jenkins-gcloud-infra/jenkins/slave")
+        def jtf = new JenkinsTerraform("${this.cwd}/jenkins-gcloud-infra/jenkins/slave")
 
         return jtf.init() + "\n" + jtf.apply("-var=instance_name=my-test-instance")
     }
