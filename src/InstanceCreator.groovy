9778d07f58fffb41490b8babf70c09bb2393143c
gespel <heimbrodt@uni-potsdam.de>
2025-03-18 11:02:20 +0100

    using terraform class now

--- src/InstanceCreator.groovy
+++ src/InstanceCreator.groovy
@@ -1,31 +1,13 @@
+import JenkinsTerraform
+
 class InstanceCreator {
     public InstanceCreator() {
 
     }
 
-    public String executeGCloudCommand(String command) {
-        def sout = new StringBuilder(), serr = new StringBuilder()
-        def proc = "/var/lib/jenkins/google-cloud-sdk/bin/gcloud ${command}".execute()
-        proc.consumeProcessOutput(sout, serr)
-        proc.waitForOrKill(100000)
-        return "$sout$serr"
-    }
-
-    public String terraformInitApply(String path) {
-        def sout_init = new StringBuilder(), serr_init = new StringBuilder(), sout_apply = new StringBuilder(), serr_apply = new StringBuilder()
-
-        def proc_init = "terraform -chdir=./${path} init".execute()
-        proc_init.consumeProcessOutput(sout_init, serr_init)
-        proc_init.waitForOrKill(1000000)
-
-        def proc_apply = "terraform -chdir=./${path} apply -var=instance_name=my-custom-vm -auto-approve".execute()
-        proc_apply.consumeProcessOutput(sout_apply, serr_apply)
-        proc_apply.waitForOrKill(1000000)
-
-        return "$sout_init$serr_init\n$sout_apply$serr_apply"
-    }
-
     public String createInstance(String name) {
-        return this.terraformInitApply("workspace/createNewVM/jenkins-gcloud-infra/jenkins/slave")
+        def jtf = new JenkinsTerraform("workspace/createNewVM/jenkins-gcloud-infra/jenkins/slave")
+
+        return jtf.init()
     }
 }
