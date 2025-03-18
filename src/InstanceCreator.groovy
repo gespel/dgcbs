723c4e69fa3d5185807eaa2420064198b9955e4a
gespel <heimbrodt@uni-potsdam.de>
2025-03-18 09:43:59 +0100

    longer waiting time

--- src/InstanceCreator.groovy
+++ src/InstanceCreator.groovy
@@ -7,7 +7,7 @@ class InstanceCreator {
         def sout = new StringBuilder(), serr = new StringBuilder()
         def proc = '/var/lib/jenkins/google-cloud-sdk/bin/gcloud compute instances list'.execute()
         proc.consumeProcessOutput(sout, serr)
-        proc.waitForOrKill(1000)
+        proc.waitForOrKill(100000)
         return "Out: $sout Error: $serr"
     }
 }
