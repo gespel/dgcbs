cf72fdf57f109e2c212a3f049ea6c023441511c8
gespel <heimbrodt@uni-potsdam.de>
2025-03-18 09:42:09 +0100

    full qualified name

--- src/InstanceCreator.groovy
+++ src/InstanceCreator.groovy
@@ -5,7 +5,7 @@ class InstanceCreator {
 
     public String createInstance() {
         def sout = new StringBuilder(), serr = new StringBuilder()
-        def proc = 'gcloud compute instances list'.execute()
+        def proc = '/var/lib/jenkins/google-cloud-sdk/bin/gcloud compute instances list'.execute()
         proc.consumeProcessOutput(sout, serr)
         proc.waitForOrKill(1000)
         return "$sout"
