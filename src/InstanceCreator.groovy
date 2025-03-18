db925eb74bf403317318906ab32a111d0120f381
gespel <heimbrodt@uni-potsdam.de>
2025-03-18 09:30:24 +0100

    groovy shell testing

--- src/InstanceCreator.groovy
+++ src/InstanceCreator.groovy
@@ -5,9 +5,9 @@ class InstanceCreator {
 
     public String createInstance() {
         def sout = new StringBuilder(), serr = new StringBuilder()
-        def proc = 'ls'.execute()
+        def proc = 'whoami'.execute()
         proc.consumeProcessOutput(sout, serr)
         proc.waitForOrKill(1000)
-        return "out> $sout\nerr> $serr"
+        return sout
     }
 }
